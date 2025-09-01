1be85747f6189921ea8b126c6d9a312a4ca44f62
MattShaw47 <explosivelemon108@gmail.com>
2025-09-01 16:27:34 -0600

    Add project files.

---
+++ HSR Overlay/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HSR_Overlay
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
